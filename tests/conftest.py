2b2f45431ce0fd16e3d128e02a93b4745897d4d5
adamgayoso <adamgayoso@users.noreply.github.com>
2023-01-29 19:24:58 -0800

    conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import shutil
-from distutils.dir_util import copy_tree
 
 import pytest
 
@@ -9,6 +8,5 @@ def save_path(tmpdir_factory):
     """Docstring for save_path."""
     dir = tmpdir_factory.mktemp("temp_data", numbered=False)
     path = str(dir)
-    copy_tree("tests/data", path)
     yield path + "/"
     shutil.rmtree(str(tmpdir_factory.getbasetemp()))
--- tests/test_visionpy.py
+++ tests/test_visionpy.py
@@ -9,9 +9,7 @@ def test_preparation(save_path):
     scanpy.settings.datasetdir = save_path
     adata = scanpy.datasets.pbmc3k_processed()
     N_SIGS = 10
-    adata.raw.varm["signatures"] = pd.DataFrame(
-        index=adata.raw.var_names, data=np.zeros((adata.raw.n_vars, N_SIGS))
-    )
+    adata.raw.varm["signatures"] = pd.DataFrame(index=adata.raw.var_names, data=np.zeros((adata.raw.n_vars, N_SIGS)))
     adata.raw.varm["signatures"].iloc[:20] = 1
     adata.raw.varm["signatures"].iloc[-25:] = -1
     adata.uns["sig_names"] = [f"Sig_{i}" for i in range(N_SIGS)]
