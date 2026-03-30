b563a427fa0c2236d6a8bf9eff10ee63716d4a55
Oier Etxezarreta <oier.etxezarreta@weizmann.ac.il>
2026-03-30 17:23:48 +0300

    utils.py file created to store the _get_mean_var function

--- src/visionpy/utils.py
+++ src/visionpy/utils.py
@@ -1,12 +1,12 @@
 import numpy as np
 from scipy import sparse
 
-def _get_mean_var(X):
+def _get_mean_var(X, axis=0):
     if sparse.issparse(X):
-        mean = np.array(X.mean(axis=0)).ravel()
-        var = np.array(X.power(2).mean(axis=0)).ravel() - mean**2
+        mean = np.array(X.mean(axis=axis)).ravel()
+        var = np.array(X.power(2).mean(axis=axis)).ravel() - mean**2
     else:
-        mean = np.mean(X, axis=0)
-        var = np.var(X, axis=0, ddof=1)
+        mean = np.mean(X, axis=axis)
+        var = np.var(X, axis=axis, ddof=1)
     return mean, var
 
